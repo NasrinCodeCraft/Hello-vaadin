148d1efad988c70dcfe702a5802575eeddc44a83
Nasrin <n.mokhtari@>
2026-02-16 11:43:17 +0330

    External CSS

--- src/main/java/com/example/hello_vaadin/MainView.java
+++ src/main/java/com/example/hello_vaadin/MainView.java
@@ -3,6 +3,7 @@ package com.example.hello_vaadin;
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.checkbox.Checkbox;
+import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -10,13 +11,16 @@ import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 
 @Route("")
+@CssImport("./styles/my-styles.css")
 public class MainView extends VerticalLayout {
     public MainView() {
         VerticalLayout todosList = new VerticalLayout();
         TextField textField = new TextField();
+        textField.addClassName("my-textfield");
         Button addButton = new Button("Add");
         addButton.addClickListener(e -> {
             Checkbox checkbox = new Checkbox(textField.getValue());
+            checkbox.addClassName("my-checkbox");
             todosList.add(checkbox);
             textField.setValue("");
         });
--- src/main/java/com/example/hello_vaadin/post/PostsView.java
+++ src/main/java/com/example/hello_vaadin/post/PostsView.java
@@ -1,5 +1,6 @@
 package com.example.hello_vaadin.post;
 
+import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.grid.GridVariant;
 import com.vaadin.flow.component.html.Div;
@@ -7,6 +8,7 @@ import com.vaadin.flow.router.Route;
 
 import java.util.List;
 
+@CssImport("./styles/my-styles.css")
 @Route("posts")
 public class PostsView extends Div {
 
@@ -14,7 +16,8 @@ public class PostsView extends Div {
     public PostsView(PostService postService) {
         setHeightFull();
         Grid<Post> grid = new Grid<>(Post.class,false);
-        grid.addColumn(Post::title).setHeader("Title");
+        grid.addClassName("my-grid");
+        grid.addColumn(Post::title).setHeader("Title").setAutoWidth(true).setFlexGrow(0);;
         grid.addColumn(Post::summary).setHeader("Summary");
         grid.addColumn(Post::url).setHeader("URL");
         grid.addColumn(Post::datePublished).setHeader("Date Published");
@@ -23,6 +26,7 @@ public class PostsView extends Div {
 
         List<Post> posts = postService.findAll();
         grid.setItems(posts);
+        grid.addThemeVariants(GridVariant.LUMO_ROW_STRIPES);
 
         add(grid);
     }
